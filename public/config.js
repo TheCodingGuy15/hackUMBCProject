76b8900ae1563a483f34cd88e5ff958c8fac9c10
TheCodingGuy15 <57414242+TheCodingGuy15@users.noreply.github.com>
2020-11-14 12:21:39 +0000

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,8 @@
+var firebaseConfig = {
+  apiKey: 'AIzaSyCc_Aj_rx23dSGL5F18CyRUSZ6cXkNDFpA',
+  authDomain: 'knockknock-7cc13.firebaseapp.com',
+  databaseURL: 'https://mealwheel-1213.firebaseio.com',
+  storageBucket: 'mealwheel-121.appspot.com',
+  projectId: 'mealwheel-1213'
+};
+firebase.initializeApp(firebaseConfig);
